00105869366503e551f831277290b0d2022be439
Ege Akman <me@egeakman.dev>
2025-03-13 20:41:42 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,14 @@ import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import metaTags from "astro-meta-tags";
 
+console.log("🔍 Build Environment:");
+console.log("READTHEDOCS:", process.env.READTHEDOCS || "Not on RTD");
+console.log("READTHEDOCS_VERSION:", process.env.READTHEDOCS_VERSION || "Not on RTD");
+
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.READTHEDOCS_VERSION ? `/${process.env.READTHEDOCS_VERSION}/` : '/',
-  site: `https://ep-website-demo.readthedocs.io${process.env.READTHEDOCS_VERSION ? `/${process.env.READTHEDOCS_VERSION}` : ''}`,
+  base: process.env.READTHEDOCS ? `/${process.env.READTHEDOCS_VERSION}/` : '/',
+  site: process.env.READTHEDOCS ? `https://ep-website-demo.readthedocs.io/${process.env.READTHEDOCS_VERSION}/` : 'https://ep2025.europython.eu',
   markdown: {
     remarkPlugins: [
       [
@@ -35,7 +39,6 @@ export default defineConfig({
       ],
     ],
   },
-  // site: "https://ep2025.europython.eu",
   integrations: [
     mdx(),
     sitemap(),
